ea5d91359f73136af933ed964461bdbe50a6b2af
OsamaAleasha <osamaalesha006@gmail.com>
2026-03-30 13:44:57 +0300

    Initial project setup

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+tasks = []
+
+@app.route("/")
+def index():
+    return render_template("index.html", tasks=tasks)
+
+
+@app.route("/add", methods=['POST'])
+def add():
+    task = request.form['task']
+
+    if task:
+        tasks.append(task)
+    return redirect('/')
+
+
+@app.route("/delete/<int:task_id>", methods=['POST'])
+def delete(task_id):
+    if 0 <= task_id < len(tasks):
+        tasks.pop(task_id)
+    return redirect('/')
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
