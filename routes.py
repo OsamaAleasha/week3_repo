a77e0ffe7018e1c4dd843ad00b3c179892e37fd8
abdallahzeine <abdallahzeine@gmail.com>
2026-04-06 13:38:05 +0300

    nn

---
+++ routes.py
@@ -0,0 +1,71 @@
+from app import db
+from app import app
+from models import Task
+from flask import  render_template, redirect, url_for, jsonify ,request
+import agent
+
+@app.route("/", methods=['GET'])
+def index():
+    tasks = Task.query.order_by(Task.id).all()
+    return render_template("index.html", tasks=tasks, ai_insight=None)
+
+@app.route("/add", methods=['POST'])
+def add():
+    task_name = request.form.get("task")
+    if task_name:
+        new_task = Task(name = task_name)
+        db.session.add(new_task)
+        db.session.commit()
+    return redirect("/")
+
+@app.route("/tasks/<int:task_id>", methods=['DELETE'])
+def delete_task(task_id):
+    task = Task.query.get(task_id)
+
+    if not task:
+        return {"error": "Task not found"}, 404
+    
+    db.session.delete(task)
+    db.session.commit()
+    return {"message": "Deleted"}
+
+@app.route("/tasks/<int:task_id>/edit", methods=['GET'])
+def edit_task(task_id):
+    task = Task.query.get(task_id)
+
+    if not task:
+        return "Task not found", 404
+    
+    return render_template("edit.html", task=task)
+
+@app.route("/tasks/<int:task_id>", methods=['PUT'])
+def update_task(task_id):
+    data = request.get_json()
+    task = Task.query.get(task_id)
+
+    if not task:
+        return {"error": "Task not found"}, 404
+    
+    task.name = data["name"]
+    db.session.commit()
+
+    
+    return {"message": "Updated", "task": task}
+
+
+@app.route("/tasks/AI", methods=['GET'])
+def AI_insights():
+    tasks = Task.query.order_by(Task.id).all()
+    if not tasks:
+        return "Tasks not found", 404
+    
+    
+    
+    task_names = [task.name for task in tasks]
+    insights_from_ai = agent.insight_task(task_names)
+
+    
+    
+    return render_template("index.html", tasks=tasks, ai_insight=insights_from_ai)
+
+
